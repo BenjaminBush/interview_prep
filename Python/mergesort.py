d9ed5f2010168a3b97e828b613ec2d799fb0d3ff
Benjamin Bush <ben.bush4@gmail.com>
2018-07-15 13:09:05 -0400

    mergesort is bae

---
+++ Python/mergesort.py
@@ -0,0 +1,39 @@
+def mergesort(alist):
+    if len(alist) > 1:
+        n = len(alist)
+        mid = n/2
+        left = alist[:mid]
+        right = alist[mid:]
+
+        mergesort(left)
+        mergesort(right)
+
+        i = 0
+        j = 0
+        k = 0
+
+        while k < len(alist) and i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                alist[k] = left[i]
+                i += 1
+            else:
+                alist[k] = right[j]
+                j += 1
+            k += 1
+
+        while k < len(alist) and i < len(left):
+            alist[k] = left[i]
+            i += 1
+            k += 1
+        
+        while k < len(alist) and j < len(right):
+            alist[k] = right[j]
+            j += 1
+            k += 1
+
+        return alist
+
+
+if __name__ == '__main__':
+    alist = [0, 9, 1, 8, 2, 7, 3, 6, 4, 5]
+    print(mergesort(alist))
